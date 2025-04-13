35d6996b7ba67ce833154f57e16d76f509777448
Sam Kurland <samkurland@Sams-MacBook-Pro-2.local>
2025-04-13 16:09:31 -0400

    addded +

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Sam Sonny</h1>
+      <h1>Sam + Sonny</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
